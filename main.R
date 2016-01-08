fa9cbbf8d708ba299cd06e4d1d80813bc969b5b2
polstein <pol.marti.sanahuja@gmail.com>
2016-01-08 12:34:00 +0100

    Missing update in this file.

--- main.R
+++ main.R
@@ -3,13 +3,10 @@ source("functions.R")
 
 ######### Main ########
 file1 = "tracks/trencacames-per-st-llorenc-i-lobac.gpx"
-track1 <- read_gpx(file1)
-#plot3d(track1$lon, track1$lat, track1$ele, type='l', col='blue')
-track1 <- smooth_gpx(track1)
-#lines3d(track1$lon, track1$lat, track1$ele, col='red')
-#plot3d(track1$lon, track1$lat, track1$ele, type='p', col='blue')
+track <- read_gpx(file1)
+track <- smooth_gpx(track)
 
-track <- track1[(!is.na(track1$lat)),]
+track <- track[(!is.na(track$lat)),]
 rownames(track) <- 1:nrow(track)
 
 track <- nearest_gpx(track)
@@ -20,17 +17,22 @@ n_track_overlap <- max(edges_clean$id_track)
 for (i in 1:(nrow(edges_clean)-1)){
   edges_clean <- rbind(edges_clean, c(edges_clean$max[i]+1, edges_clean$min[i+1]-1, i+n_track_overlap))  
 }
+rownames(edges_clean) <- 1:nrow(edges_clean)
 
-#mask <- c()
-#for (i in 1:nrow(edges_clean)){
-#  mask <- c(mask, edges_clean$min[i]:edges_clean$max[i])
-#}
+n_track_overlap <- max(edges_clean$id_track)
 
-#track_clean <- track[setdiff(1:nrow(track), mask),]
-#plot3d(track$lon, track$lat, track$ele, type='l', col='blue')
-plot3d(track_clean$lon, track_clean$lat, track_clean$ele, type='p', col='red')
+df_edges <- data.frame(min=numeric(n_track_overlap), max=numeric(n_track_overlap),  id_track=numeric(n_track_overlap) )
+for (i in 1:n_track_overlap){
+  df_edges[i,] <- edges_clean[edges_clean$id_track==i,][1,]
+}
 
-
-#file2 = "tracks/hospital-de-sang-serra-de-lobac.gpx"
-#track2 <- read_gpx(file2)
-#lines3d(track2$lon, track2$lat, track2$ele, col='red')
+id_points <- df_edges$min[1]:df_edges$max[1]
+plot3d(track$lon[id_points], track$lat[id_points], track$ele[id_points], type='l', col=palette()[1], 
+       xlim=c(min(track$lon),max(track$lon)),
+       ylim=c(min(track$lat),max(track$lat)),
+       zlim=c(min(track$ele),max(track$ele))
+       )
+for (i in 2:nrow(df_edges)){
+  id_points <- df_edges$min[i]:df_edges$max[i]
+  lines3d(track$lon[id_points], track$lat[id_points], track$ele[id_points],  col=palette()[i])
+}
