ca951ada5a937b22084e291910bcc79f50d0adea
polstein <pol.marti.sanahuja@gmail.com>
2016-01-14 14:14:41 +0100

    wikiloc_scrap.R no generates a scatter plot with hundreds of tracks from Collserola.

--- wikiloc_scrap.R
+++ wikiloc_scrap.R
@@ -1,20 +1,43 @@
 library(rvest)
 library("RJSONIO")
+library("TTR")
+library(XML)
+library(rgl)
 
-track <- read_html("http://es.wikiloc.com/wikiloc/view.do?id=2944224")
+for (i in seq(10,1000,10)){
+  print(i)
+  
+  wikiloc <- read_html(paste0("http://es.wikiloc.com/wikiloc/find.do?act=1%2C&q=collserola&from=", i-10,"&to=",i))
+  
+  track_link <- wikiloc %>% 
+       html_nodes("h3") %>%
+       html_nodes("a") %>% html_attr("href") 
+  
+  for (j in 1:10){
+    print(j)
+    
+    track <- read_html(track_link[j])
+    
+    a <- track %>% 
+      html_nodes("div") %>%
+      html_nodes("script") 
+    k <- grep('lat',html_text(a))
+    a <- a[k]
+    k <- grep('trinfo',html_text(a))
+    a <-html_text(a[k])
+    a <- gsub("\n\t\t\t\t\t\tvar trinfo =", "",  a)
+    a <- gsub(";\n\t\t\t\t\t\t", "", a)
+    a <- fromJSON(a)
+    
+    lon <- a$at$n
+    ele <- a$at$l
+    lat <- a$at$a
+    
+    #print(length(lon))
+    
+    if((i==10) & (j==1)){track_base <- data.frame(lat = lat, lon = lon, ele = ele)
+    }else{track_base <- rbind(track_base, data.frame(lat = lat, lon = lon, ele = ele))}
+  }
+}
 
-a <- track %>% 
-  html_nodes("script")
-a <- a[3]
-a <-html_text(a)
-a <- gsub("\n\t\t\t\t\t\tvar trinfo =", "",  a)
-a <- gsub(";\n\t\t\t\t\t\t", "", a)
-a <- fromJSON(a)
-
-ele <- a$at$n
-lon <- a$at$l
-lat <- a$at$a
-
-track <- data.frame(lat = lat, lon = lon, ele = ele)
-
-plot3d(track$lon, track$lat, track$ele, type='p',  col='blue')
+plot(track_base$lon, track_base$lat, type='p',  col='blue', pch='.')
